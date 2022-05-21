15ab684e72f0c6f2dd3923eb8f1582cbfdc0c6b9
Jimena Cambronero <jimenacambronero@gmail.com>
2022-05-20 23:44:53 -0300

    Carrito

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -8,54 +8,45 @@ const Cart = () => {
     const { cartList, emptyCart, totalPrice } = useCartContext();
   
     return (
-
-      <div className='flex flex-col justify-between w-full p-4 lg:max-w-7xl xl:m-auto'>
-         <h2 className='self-center mb-6 text-2xl leading-tight tracking-wider text-center text-gray-800 uppercase font-amastic '>Mi Carrito</h2>
-   
-         <table className='p-10'> 
-         <thead className='h-10 text-center'>
-          <tr className='border-b border-gray-300'>
-            <th className='pl-4 text-xl leading-loose tracking-wider text-left text-gray-700 uppercase font-amastic'>
-              Producto
-            </th>
-            <th className='px-6 text-xl tracking-wide text-gray-800 font-amastic lg:px-16 xl:px-32'>
-              Precio
-            </th>
-            <th className='px-6 text-xl tracking-wide text-gray-800 font-amastic md:px-2 lg:px-8 xl:px-12'>
-              Cantidad
-            </th>
-            <th className='px-6 text-xl tracking-wide text-gray-800 font-amastic md:px-2 lg:px-16 xl:px-28'>
-              Subtotal
-            </th>
-            <th className='px-6 text-xl tracking-wide text-gray-800 font-amastic md:px-2 lg:px-16 xl:px-28'>
-              Por producto
-            </th>
-            <th className='px-6 text-xl tracking-wide text-gray-800 font-amastic md:px-2 lg:px-16 xl:px-28'>
-              Todos los productos
-            </th>
+    <div>
+      <h2 className='self-center p-8 text-2xl leading-tight tracking-wider text-center text-gray-800 uppercase font-amastic'>Mi Carrito</h2>
+      <table className='w-full table-fixed'>
+        <thead>
+          <tr className='border-b border-gray-200'>
+            <th className='items-center text-xl tracking-wide text-gray-800 font-amastic'>Producto</th>
+            <th className='items-center text-xl tracking-wide text-gray-800 font-amastic'>Precio</th>
+            <th className='items-center text-xl tracking-wide text-gray-800 font-amastic'>Cantidad</th>
+            <th className='items-center text-xl tracking-wide text-gray-800 font-amastic'>Subtotal</th>
+            <th className='items-center text-xl tracking-wide text-gray-800 font-amastic'>Por Producto</th>
+            <th className='items-center text-xl tracking-wide text-gray-800 font-amastic'>Todos los Productos</th>
           </tr>
         </thead>
-        </table>
-        
-         
-        {cartList.map((item) => (
-          <CartItem key={item.id} item={item} />
-        ))}
-        
-        {cartList.length > 0 ? (
-          <>
-            <button onClick={() => emptyCart()}>Vaciar Carrito</button>
-            <h1>Precio total: ${totalPrice()}</h1>
-          </>
-        ) : (
-          <CarritoVacio/>
-        )}
-      </div>
+     
+      <tbody>
+      </tbody>
+      {cartList.map((item) => (
+        <CartItem key={item.id} item={item} />
+      ))}
+       </table>
+      {cartList.length > 0 ? (
+        <>
+        <div className='flex flex-col items-end pr-40' >
+
+          <button className='text-xl tracking-wide text-gray-700 font-boogaloo' onClick={() => emptyCart()}>Vaciar Carrito</button>
+          <p className='text-xl tracking-wide text-gray-700 font-boogaloo'> Precio total: ${totalPrice()}</p>
+
+        </div>
+        </>   
+      ) : (
+        <CarritoVacio/>
+      )}
+     
+    </div>
+
      
     );
   };
   
   export default Cart;
-
 
 
--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -2,102 +2,40 @@ import React from 'react'
 import { useCartContext } from '../context/CartContext'
 import {RiDeleteBinFill} from 'react-icons/ri'
 
+
 const CartItem = ({ item }) => {
   const { deleteById, removeOneUnit } = useCartContext();
 
   const { name, quantity, price, id, img,} = item;
 
   return (
-    // <div className='mx-10'>
-    //   <div className='flex flex-col justify-between w-full p-4 lg:max-w-7xl xl:m-auto'>
-    // <table className='overflow-x-auto whitespace-nowrap'> 
+    <tr className='border-b border-gray-200'>
+      <th className='flex flex-col items-center pr-40 justify-left md:pr-0'> 
+      <img className='h-32 w-36'src={img} alt="simon cat imagen" />
+      <div className='flex flex-col text-left'>
+        <p className='text-lg tracking-wide text-gray-700 font-amastic'>{name}</p>
+      </div>
+      </th>
+      <th className='text-lg tracking-wide text-gray-700 font-amastic'>
+      {price}
+      </th>
+      <th className='text-lg tracking-wide text-gray-700 font-amastic'>
+      {quantity}
+      </th>
+      <th className='text-lg tracking-wide text-gray-700 font-amastic'>
+      {quantity * price}
+      </th>
+      <th>
+       <button className='text-lg'  onClick={() => removeOneUnit(id)}><RiDeleteBinFill/></button>
+    </th>
+     <th>
+      <button className='px-6 text-lg md:px-2 lg:px-16 xl:px-28'onClick={() => deleteById(id)}><RiDeleteBinFill/></button>
+       </th>
+ </tr>
+     );
+    };
+    
+    export default CartItem;  
 
-<div className='flex flex-col justify-between w-full p-4 lg:max-w-7xl xl:m-auto'>
-
-
-    <table className=''> 
-         <thead className='h-10 text-center'>
-          <tr className='border-b border-gray-300'>
-          <th className='flex flex-row items-center'>
-              <img className='h-32 pr-6 w-36'src={img} alt="simon cat imagen" />
-                <div className='flex flex-col text-left'>
-                <p className='text-lg tracking-wide text-gray-700 font-amastic'> {name}
-                </p>
-                </div>
-            </th>
-            <th className='px-6 text-lg tracking-wide text-gray-700 font-amastic ' >
-             ${price}
-            </th>
-            <th className='text-lg tracking-wide text-gray-700 font-amastic'> 
-            {quantity}
-            </th>
-            <th className='text-lg tracking-wide text-gray-700 font-amastic'> 
-            {quantity}
-            </th>
-            <th>
-            <button className='text-lg' onClick={() => removeOneUnit(id)}><RiDeleteBinFill/></button>
-            </th>
-            <th>
-            <button className='text-lg'onClick={() => deleteById(id)}><RiDeleteBinFill/></button>
-            </th>
-          </tr>
-        </thead>
-        </table>
-
-
-        </div>
-
-
-
-
-
-        
-        /* <tbody className='w-full text-center'>
-          <tr className='border-b border-gray-200'>
-            
-            <th className='flex flex-row items-center'>
-              <img className='h-32 pr-6 w-36'src={img} alt="simon cat imagen" />
-                <div className='flex flex-col text-left'>
-                <p className='text-lg tracking-wide text-gray-700 font-amastic'> {name}
-                </p>
-                </div>
-            </th>
-
-            <th className='text-lg tracking-wide text-gray-700 font-amastic '>
-             ${price}
-            </th>
-
-            <th className='text-lg tracking-wide text-gray-700 font-amastic'> 
-            {quantity}
-            </th>
-
-            <th className='text-lg tracking-wide text-gray-700 font-amastic'> 
-            {quantity}
-            </th>
-
-            <th>
-            <button className='text-lg' onClick={() => removeOneUnit(id)}><RiDeleteBinFill/></button>
-            </th>
-            
-            <th>
-            <button className='text-lg'onClick={() => deleteById(id)}><RiDeleteBinFill/></button>
-            </th>
-          </tr>
-        </tbody> */
-    // </table>
-  
-     
-     
-
-    // </div>
-    // </div>
-
- 
-
-   
-  );
-};
-
-export default CartItem;
 
 
