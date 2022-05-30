efee8028fb3cb0316f57c418cc90996ba7b638c7
Jimena Cambronero <jimenacambronero@gmail.com>
2022-05-30 19:12:09 -0300

    pages

--- src/Page/NotFound404.jsx
+++ src/Page/NotFound404.jsx
@@ -1,10 +1,15 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react'
 
 const NotFound404 = () => {
   return (
-    <div>
-        <h1>Pagina no encontrada</h1>
-        <p>Error 404</p>
+    <div className="flex flex-col items-center justify-center max-w-full p-20 mx-auto lg:max-w-7xl lg:items-star">
+        <div>
+        <iframe src="https://giphy.com/embed/1URlthYDD9ZfNe68JT" width="480" height="480" frameBorder="0" allowFullScreen></iframe>
+        </div>
+        <div>
+            <h4 className="p-4 text-lg tracking-wider uppercase text-black-500 font-shadows">No pudimos encontrar la Página Buscada... Podes intentarlo nuevamente</h4>
+        </div>
     </div>
   )
 }
